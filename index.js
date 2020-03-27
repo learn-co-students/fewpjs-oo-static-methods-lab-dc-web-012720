3a41d654a211fd24bddc6e79dd406f5fd0ec91e2
Lauren Yu <laurenkyu@gmail.com>
2020-03-27 18:47:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let array = string.split("")
+    array[0] = array[0].toUpperCase()
+    let newArray = array.join("")
+    return newArray
+  }
+  
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  
+  static titleize(string) {
+    let array = string.split(" ")
+    let newArray = array.map((word) => {
+      if (word == array[0]) {
+        return this.capitalize(word)
+      } else if (word == "a" || word == "an" || word == "but" || word == "of" || word == "and" || word == "for" || word == "at" || word == "by" || word == "from" || word == "the"){
+        return word
+      } else {
+      return this.capitalize(word)}
+    })
+    return newArray.join(" ")
+  }
 }
+
+console.log(Formatter.titleize("i am hungry"))
+
+// console.log(Formatter.capitalize("hello"))
